56351a60425454397cb3cbc54a70aea9c9b1c8c3
mdeverdelhan <marc.deverdelhan@yahoo.com>
2014-05-23 15:48:30 +0200

    Adding first raw TTB

---
+++ src/main/java/misc/TTB.java
@@ -0,0 +1,151 @@
+package misc;
+
+import com.xeiam.xchange.Exchange;
+import com.xeiam.xchange.ExchangeFactory;
+import com.xeiam.xchange.bitstamp.BitstampExchange;
+import com.xeiam.xchange.bitstamp.service.streaming.BitstampStreamingConfiguration;
+import com.xeiam.xchange.dto.marketdata.Trade;
+import com.xeiam.xchange.service.streaming.ExchangeEvent;
+import com.xeiam.xchange.service.streaming.StreamingExchangeService;
+import eu.verdelhan.ta4j.OperationType;
+import eu.verdelhan.ta4j.Strategy;
+import eu.verdelhan.ta4j.TimeSeries;
+import eu.verdelhan.ta4j.indicators.oscillators.StochasticOscillatorDIndicator;
+import eu.verdelhan.ta4j.indicators.oscillators.StochasticOscillatorKIndicator;
+import eu.verdelhan.ta4j.indicators.simple.ClosePriceIndicator;
+import eu.verdelhan.ta4j.indicators.trackers.EMAIndicator;
+import eu.verdelhan.ta4j.indicators.trackers.MACDIndicator;
+import eu.verdelhan.ta4j.series.DefaultTimeSeries;
+import eu.verdelhan.ta4j.strategies.AlwaysOperateStrategy;
+import eu.verdelhan.ta4j.strategies.CombinedBuyAndSellStrategy;
+import eu.verdelhan.ta4j.strategies.IndicatorOverIndicatorStrategy;
+import eu.verdelhan.ta4j.strategies.ResistanceStrategy;
+import eu.verdelhan.ta4j.strategies.SupportStrategy;
+import eu.verdelhan.ta4j.ticks.DefaultTick;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.joda.time.DateTime;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+public class TTB {
+
+    private static Twitter TWITTER = TwitterFactory.getSingleton();
+
+    private static int TICK_PERIOD = 300;
+
+    private static int MAX_NB_TICKS = 80;
+
+    private static ArrayList<DefaultTick> TICKS = new ArrayList<DefaultTick>();
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        Exchange bitstamp = ExchangeFactory.INSTANCE.createExchange(BitstampExchange.class.getName());
+        BitstampStreamingConfiguration streamCfg = new BitstampStreamingConfiguration();
+
+        // Interested in the public streaming market data feed (no authentication)
+        StreamingExchangeService streamService = bitstamp.getStreamingExchangeService(streamCfg);
+
+        streamService.connect();
+        runBot(streamService);
+        streamService.disconnect();
+    }
+
+    private static Strategy buildStrategy(TimeSeries series) {
+        if (series == null) {
+            throw new IllegalArgumentException("Series cannot be null");
+        }
+
+        ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
+        EMAIndicator shortEma = new EMAIndicator(closePrice, 9);
+        EMAIndicator longEma = new EMAIndicator(closePrice, 26);
+
+        IndicatorOverIndicatorStrategy shortEmaAboveLongEma = new IndicatorOverIndicatorStrategy(longEma, shortEma);
+
+        StochasticOscillatorKIndicator stochasticOscillK = new StochasticOscillatorKIndicator(series, 14);
+        StochasticOscillatorDIndicator stochasticOscillD = new StochasticOscillatorDIndicator(stochasticOscillK);
+
+        SupportStrategy support20 = new SupportStrategy(stochasticOscillK, new AlwaysOperateStrategy().opposite(), 20);
+        ResistanceStrategy resist80 = new ResistanceStrategy(stochasticOscillK, new AlwaysOperateStrategy().opposite(), 80);
+
+        MACDIndicator macd = new MACDIndicator(closePrice, 9, 26);
+        EMAIndicator emaMacd = new EMAIndicator(macd, 18);
+
+        IndicatorOverIndicatorStrategy macdAboveSignaLine = new IndicatorOverIndicatorStrategy(emaMacd, macd);
+
+        return shortEmaAboveLongEma
+                .and(new CombinedBuyAndSellStrategy(support20, resist80))
+                .and(macdAboveSignaLine);
+    }
+
+    private static void runBot(StreamingExchangeService stream) throws IOException {
+
+        try {
+            for (;;) {
+                ExchangeEvent evt = stream.getNextEvent();
+                switch (evt.getEventType()) {
+                    case TRADE:
+                        addTransactionToTicks((Trade) evt.getPayload());
+                        TimeSeries series = new DefaultTimeSeries(TICKS);
+                        Strategy strategy = buildStrategy(series);
+                        boolean buy = strategy.shouldEnter(series.getEnd());
+                        boolean sell = strategy.shouldExit(series.getEnd());
+                        if (buy && !sell) {
+                            tweet(OperationType.BUY);
+                        } else if (!buy && sell) {
+                            tweet(OperationType.SELL);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    private static void addTransactionToTicks(Trade trade) {
+        DateTime now = new DateTime();
+        if (TICKS.isEmpty()) {
+            // No tick yet
+            DefaultTick newTick = new DefaultTick(now, now.plusSeconds(TICK_PERIOD));
+            newTick.addTrade(trade.getTradableAmount().doubleValue(), trade.getPrice().doubleValue());
+            TICKS.add(newTick);
+        } else {
+            DefaultTick lastTick = TICKS.get(TICKS.size()-1);
+            if (lastTick.inPeriod(now)) {
+                // In the last tick
+                lastTick.addTrade(trade.getTradableAmount().doubleValue(), trade.getPrice().doubleValue());
+            } else {
+                // Out of the last tick
+                DefaultTick newTick = new DefaultTick(now, now.plusSeconds(TICK_PERIOD));
+                newTick.addTrade(trade.getTradableAmount().doubleValue(), trade.getPrice().doubleValue());
+                TICKS.add(newTick);
+            }
+        }
+
+        if (TICKS.size() > MAX_NB_TICKS) {
+            TICKS.remove(0);
+        }
+    }
+
+    private static void tweet(OperationType operationType) {
+        if (operationType != null) {
+            try {
+                if (operationType == OperationType.BUY) {
+                    TWITTER.updateStatus("Buy signal on #Bitstamp! #bitcoin #btc /cc @MarcdeVerdelhan");
+                } else {
+                    TWITTER.updateStatus("Sell signal on #Bitstamp! #bitcoin #btc /cc @MarcdeVerdelhan");
+                }
+            } catch (TwitterException te) {
+                te.printStackTrace();
+            }
+        }
+    }
+}
