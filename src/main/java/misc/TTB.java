d017b3f942287f579beb9d0f2c7981cf076e87b0
mdeverdelhan <marc.deverdelhan@yahoo.com>
2014-06-07 21:38:42 +0200

    Testing XChange snapshot after live trades addition

--- src/main/java/misc/TTB.java
+++ src/main/java/misc/TTB.java
@@ -1,5 +1,14 @@
 package misc;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.joda.time.DateTime;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
 import com.xeiam.xchange.Exchange;
 import com.xeiam.xchange.ExchangeFactory;
 import com.xeiam.xchange.bitstamp.BitstampExchange;
@@ -7,6 +16,7 @@ import com.xeiam.xchange.bitstamp.service.streaming.BitstampStreamingConfigurati
 import com.xeiam.xchange.dto.marketdata.Trade;
 import com.xeiam.xchange.service.streaming.ExchangeEvent;
 import com.xeiam.xchange.service.streaming.StreamingExchangeService;
+
 import eu.verdelhan.ta4j.OperationType;
 import eu.verdelhan.ta4j.Strategy;
 import eu.verdelhan.ta4j.TimeSeries;
@@ -22,12 +32,6 @@ import eu.verdelhan.ta4j.strategies.IndicatorOverIndicatorStrategy;
 import eu.verdelhan.ta4j.strategies.ResistanceStrategy;
 import eu.verdelhan.ta4j.strategies.SupportStrategy;
 import eu.verdelhan.ta4j.ticks.DefaultTick;
-import java.io.IOException;
-import java.util.ArrayList;
-import org.joda.time.DateTime;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
 
 public class TTB {
 
@@ -39,17 +43,18 @@ public class TTB {
 
     private static ArrayList<DefaultTick> TICKS = new ArrayList<DefaultTick>();
 
-
-
     public static void main(String[] args) throws IOException {
-
+        System.out.println("STARTED");
         Exchange bitstamp = ExchangeFactory.INSTANCE.createExchange(BitstampExchange.class.getName());
         BitstampStreamingConfiguration streamCfg = new BitstampStreamingConfiguration();
+        streamCfg.getChannels().add("live_trades");
 
-        // Interested in the public streaming market data feed (no authentication)
+        // Interested in the public streaming market data feed (no
+        // authentication)
         StreamingExchangeService streamService = bitstamp.getStreamingExchangeService(streamCfg);
 
         streamService.connect();
+        System.out.println("CONNECTED");
         runBot(streamService);
         streamService.disconnect();
     }
@@ -69,16 +74,15 @@ public class TTB {
         StochasticOscillatorDIndicator stochasticOscillD = new StochasticOscillatorDIndicator(stochasticOscillK);
 
         SupportStrategy support20 = new SupportStrategy(stochasticOscillK, new AlwaysOperateStrategy().opposite(), 20);
-        ResistanceStrategy resist80 = new ResistanceStrategy(stochasticOscillK, new AlwaysOperateStrategy().opposite(), 80);
+        ResistanceStrategy resist80 = new ResistanceStrategy(stochasticOscillK, new AlwaysOperateStrategy().opposite(),
+                80);
 
         MACDIndicator macd = new MACDIndicator(closePrice, 9, 26);
         EMAIndicator emaMacd = new EMAIndicator(macd, 18);
 
         IndicatorOverIndicatorStrategy macdAboveSignaLine = new IndicatorOverIndicatorStrategy(emaMacd, macd);
 
-        return shortEmaAboveLongEma
-                .and(new CombinedBuyAndSellStrategy(support20, resist80))
-                .and(macdAboveSignaLine);
+        return shortEmaAboveLongEma.and(new CombinedBuyAndSellStrategy(support20, resist80)).and(macdAboveSignaLine);
     }
 
     private static void runBot(StreamingExchangeService stream) throws IOException {
@@ -87,20 +91,23 @@ public class TTB {
             for (;;) {
                 ExchangeEvent evt = stream.getNextEvent();
                 switch (evt.getEventType()) {
-                    case TRADE:
-                        addTransactionToTicks((Trade) evt.getPayload());
-                        TimeSeries series = new DefaultTimeSeries(TICKS);
-                        Strategy strategy = buildStrategy(series);
-                        boolean buy = strategy.shouldEnter(series.getEnd());
-                        boolean sell = strategy.shouldExit(series.getEnd());
-                        if (buy && !sell) {
-                            tweet(OperationType.BUY);
-                        } else if (!buy && sell) {
-                            tweet(OperationType.SELL);
-                        }
-                        break;
-                    default:
-                        break;
+                case TRADE:
+                    System.out.println("NEW TRADE");
+                    addTransactionToTicks((Trade) evt.getPayload());
+                    TimeSeries series = new DefaultTimeSeries(TICKS);
+                    Strategy strategy = buildStrategy(series);
+                    boolean buy = strategy.shouldEnter(series.getEnd());
+                    boolean sell = strategy.shouldExit(series.getEnd());
+                    if (buy && !sell) {
+                        System.out.println("BUY");
+                        // tweet(OperationType.BUY);
+                    } else if (!buy && sell) {
+                        System.out.println("SELL");
+                        // tweet(OperationType.SELL);
+                    }
+                    break;
+                default:
+                    break;
                 }
 
             }
@@ -118,7 +125,7 @@ public class TTB {
             newTick.addTrade(trade.getTradableAmount().doubleValue(), trade.getPrice().doubleValue());
             TICKS.add(newTick);
         } else {
-            DefaultTick lastTick = TICKS.get(TICKS.size()-1);
+            DefaultTick lastTick = TICKS.get(TICKS.size() - 1);
             if (lastTick.inPeriod(now)) {
                 // In the last tick
                 lastTick.addTrade(trade.getTradableAmount().doubleValue(), trade.getPrice().doubleValue());
